7f2fd9b88591be8d4569a2cff9d185dfdd6c606c
ambikagithub <108048651+ambikagithub@users.noreply.github.com>
2022-08-10 14:17:31 +0530

    updated new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld
     public static void main(String args[])
 {  
      System.out.println("Hello Agni");
+        System.out.println("Hello Bi");
+        System.out.println("Hello C");
 System.out.println("Hello Riya");  
     }  
 }   
